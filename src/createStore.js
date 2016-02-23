dbcc9ed158f56bc6d0a99998066c7a3a9a41d348
Mike Kruk <tamagokun@gmail.com>
2016-02-23 12:17:12 -0500

    use react-router-redux middleware for action creator based routing

--- src/createStore.js
+++ src/createStore.js
@@ -1,10 +1,11 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux'
-import { routerReducer as routing } from 'react-router-redux'
+import { routerReducer as routing, routerMiddleware } from 'react-router-redux'
+import { browserHistory } from 'react-router'
 
 export default (middleware = [], reducers = {}, enhancers = [], hot = null) => {
   const reducer = combineReducers({...reducers, routing})
   const store = createStore(reducer, compose(
-    applyMiddleware(...middleware),
+    applyMiddleware(...middleware, routerMiddleware(browserHistory)),
     ...enhancers,
     typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
   ))
